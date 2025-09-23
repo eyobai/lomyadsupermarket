3768d2314ab4817257845fee4539498cd09bc0a1
eyobai <eyobsisayy@gmail.com>
2025-09-23 12:31:20 +0300

    email updated in the footer section

--- src/app/Footer.tsx
+++ src/app/Footer.tsx
@@ -54,8 +54,8 @@ export default function Footer() {
                   <path d="M2.003 5.884L10 9.882l7.997-3.998A2 2 0 0016 4H4a2 2 0 00-1.997 1.884z"/>
                   <path d="M18 8.118l-8 4-8-4V14a2 2 0 002 2h12a2 2 0 002-2V8.118z"/>
                 </svg>
-                <a href="mailto:info@lomyadsupermarket.com" className="hover:text-green-200 transition-colors">
-                  info@lomyadsupermarket.com
+                <a href="mailto: lomyadsupermarket@gmail.com" className="hover:text-green-200 transition-colors">
+                lomyadsupermarket@gmail.com
                 </a>
               </p>
               <p className="flex items-center">
